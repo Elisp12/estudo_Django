bb38a8c1849f6ba2beb7bd579bea73c3bb14a444
Eliseu Santos Pinto <eliseuspdam.1999@gmail.com>
2024-06-19 20:32:59 -0300

    inserting data into the home list

--- base/views.py
+++ base/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from utils.parts.factory import make_parts
 
 # Create your views here.
 
 def home (request):
-    return render(request,'base-home/pages/home.html', context=
-        {'name':'Eliseu',
+    return render(request,'base-home/pages/home.html', context= {
+        'parts': [make_parts() for _ in range(10)],
 }) 
 
 def pice (request, id):
-    return render(request,'base-home/pages/list_view.html', context=
-        {'name':'Eliseu',
+    return render(request,'base-home/pages/list_view.html', context={
+        'part': [make_parts],
 })
