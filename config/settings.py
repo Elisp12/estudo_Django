2477f5799848dbe62a69c73654532e254d9c4cca
Eliseu Santos Pinto <eliseuspdam.1999@gmail.com>
2024-06-19 18:23:52 -0300

    adding and configuring templestes_base

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,11 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+
+        'DIRS': [
+            BASE_DIR / 'base_templetes',
+        ],
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
