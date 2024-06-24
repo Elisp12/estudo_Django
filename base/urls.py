25d17cb3e16801178797f195ce88a0c933774372
Eliseu Santos Pinto <eliseuspdam.1999@gmail.com>
2024-06-24 18:37:51 -0300

    name_app, unique name in url

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'parts'
 
 urlpatterns = [
-    path('', views.home), # home
-    path('part/<int:id>/', views.parts), # part
+    path('', views.home, name='home'), # home
+    path('part/<int:id>/', views.parts, name='part'), # part
    
 ]
